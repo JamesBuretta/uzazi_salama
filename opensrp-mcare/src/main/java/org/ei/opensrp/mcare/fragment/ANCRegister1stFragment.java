12085170220fe8ce6bc69d581185fe517329f048
Ali Suleiman <allysuley@gmail.com>
2017-08-22 19:35:20 +0300

    Change androidToolsBuildGradle and androidBuildToolsVersion  to 25.0.0 and add material date picker librabry

--- opensrp-mcare/src/main/java/org/ei/opensrp/mcare/fragment/ANCRegister1stFragment.java
+++ opensrp-mcare/src/main/java/org/ei/opensrp/mcare/fragment/ANCRegister1stFragment.java
@@ -7,9 +7,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import org.ei.opensrp.mcare.R;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,6 +22,10 @@ import org.ei.opensrp.mcare.R;
 public class ANCRegister1stFragment extends Fragment {
 
     TextView textDate;
+    LinearLayout layoutDatePick;
+
+    private Calendar today;
+    private SimpleDateFormat dateFormat = new SimpleDateFormat("dd MMM yyyy", Locale.getDefault());
 
     public ANCRegister1stFragment() {
         // Required empty public constructor
@@ -25,6 +34,7 @@ public class ANCRegister1stFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        today = Calendar.getInstance();
     }
 
     @Override
@@ -34,6 +44,17 @@ public class ANCRegister1stFragment extends Fragment {
         View fragmentView = inflater.inflate(R.layout.fragment_ancregister1st, container, false);
 
         textDate = (TextView) fragmentView.findViewById(R.id.textDate);
+        layoutDatePick = (LinearLayout) fragmentView.findViewById(R.id.layoutDatePick);
+
+        // initialize date to today's date
+        textDate.setText(dateFormat.format(today.getTimeInMillis()));
+
+        layoutDatePick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // pick date
+            }
+        });
 
 
         return fragmentView;
