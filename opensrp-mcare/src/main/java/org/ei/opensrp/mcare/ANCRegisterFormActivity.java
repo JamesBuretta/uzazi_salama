bd66a7827b2183f94cc832f8e3fc8a9b6d1fa09c
Ali Suleiman <allysuley@gmail.com>
2017-08-22 07:09:06 +0300

    Add Submit Form FAB along with its hide/show animations

--- opensrp-mcare/src/main/java/org/ei/opensrp/mcare/ANCRegisterFormActivity.java
+++ opensrp-mcare/src/main/java/org/ei/opensrp/mcare/ANCRegisterFormActivity.java
@@ -1,9 +1,12 @@
 package org.ei.opensrp.mcare;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 import org.ei.opensrp.mcare.R;
 import org.ei.opensrp.mcare.adapter.ANCRegisterPagerAdapter;
@@ -11,6 +14,11 @@ import org.ei.opensrp.mcare.adapter.ANCRegisterPagerAdapter;
 public class ANCRegisterFormActivity extends ActionBarActivity {
 
     private ViewPager viewPager;
+    Animation animationFabShow, animationFabHide, animationFabHideSlow;
+    FloatingActionButton fabDone;
+
+    private ANCRegisterPagerAdapter pagerAdapter;
+    private TabLayout tabs;
 
     @SuppressWarnings("deprecation")
     @Override
@@ -18,31 +26,57 @@ public class ANCRegisterFormActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ancregister_form);
 
+        animationFabShow = AnimationUtils.loadAnimation(getApplicationContext(),
+                R.anim.anim_fab_show_fast);
+        animationFabHide = AnimationUtils.loadAnimation(getApplicationContext(),
+                R.anim.anim_fab_hide_fast);
+        animationFabHideSlow = AnimationUtils.loadAnimation(getApplicationContext(),
+                R.anim.anim_fab_hide);
+
+
+        fabDone = (FloatingActionButton) findViewById(R.id.fabDone);
+
         viewPager = (ViewPager) findViewById(R.id.viewPager);
-        ANCRegisterPagerAdapter pagerAdapter
-                = new ANCRegisterPagerAdapter(getSupportFragmentManager());
+        pagerAdapter = new ANCRegisterPagerAdapter(getSupportFragmentManager());
 
         viewPager.setAdapter(pagerAdapter);
 
         viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
             public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
             }
 
             @Override
             public void onPageSelected(int position) {
 
+                if (position == pagerAdapter.getCount() - 1) {
+                    // show only on the last fragment
+                    fabDone.startAnimation(animationFabShow);
+                    fabDone.setEnabled(true);
+
+                } else if (fabDone.isEnabled()) {
+                    fabDone.startAnimation(animationFabHide);
+                    fabDone.setEnabled(false);
+                }
             }
 
             @Override
             public void onPageScrollStateChanged(int state) {
-
             }
         });
 
-
-        TabLayout tabs = (TabLayout) findViewById(R.id.tabs);
+        tabs = (TabLayout) findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (fabDone.isEnabled() &&
+                tabs.getSelectedTabPosition() != pagerAdapter.getCount() - 1) {
+            // hide fab on starting activity
+            fabDone.startAnimation(animationFabHideSlow);
+            fabDone.setEnabled(false);
+        }
     }
 }
