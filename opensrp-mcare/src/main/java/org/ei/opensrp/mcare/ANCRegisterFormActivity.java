8b2d9d938a6dbf9949d49afb6ac69fe19e66bd46
Ali Suleiman <allysuley@gmail.com>
2017-08-22 05:19:59 +0300

    Modify ANCRegisterFormActivity layout to use ViewPager

--- opensrp-mcare/src/main/java/org/ei/opensrp/mcare/ANCRegisterFormActivity.java
+++ opensrp-mcare/src/main/java/org/ei/opensrp/mcare/ANCRegisterFormActivity.java
@@ -1,5 +1,6 @@
 package org.ei.opensrp.mcare;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 
@@ -7,9 +8,13 @@ import org.ei.opensrp.mcare.R;
 
 public class ANCRegisterFormActivity extends ActionBarActivity {
 
+    private ViewPager viewPager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ancregister_form);
+
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
     }
 }
