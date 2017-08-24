1498c98c1c00d499c40fb8a6be1a30c2dab95bad
Ali Suleiman <allysuley@gmail.com>
2017-08-24 16:00:35 +0300

    Add modifications to registration form

--- opensrp-mcare/src/main/java/org/ei/opensrp/mcare/fragment/ANCRegister1stFragment.java
+++ opensrp-mcare/src/main/java/org/ei/opensrp/mcare/fragment/ANCRegister1stFragment.java
@@ -1,13 +1,17 @@
 package org.ei.opensrp.mcare.fragment;
 
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.CardView;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -17,7 +21,7 @@ import org.ei.opensrp.mcare.R;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.Locale;
 
 /**
@@ -25,8 +29,9 @@ import java.util.Locale;
  */
 public class ANCRegister1stFragment extends Fragment {
 
-    TextView textDate;
-    LinearLayout layoutDatePick;
+    TextView textDate, textPhone, textDateLNMP;
+    LinearLayout layoutDatePick, layoutEditPhone;
+    CardView cardDatePickLNMP;
 
     private Calendar today;
     private SimpleDateFormat dateFormat = new SimpleDateFormat("dd MMM yyyy", Locale.getDefault());
@@ -48,7 +53,11 @@ public class ANCRegister1stFragment extends Fragment {
         View fragmentView = inflater.inflate(R.layout.fragment_ancregister1st, container, false);
 
         textDate = (TextView) fragmentView.findViewById(R.id.textDate);
+        textPhone = (TextView) fragmentView.findViewById(R.id.textPhone);
+        textDateLNMP = (TextView) fragmentView.findViewById(R.id.textDateLNMP);
         layoutDatePick = (LinearLayout) fragmentView.findViewById(R.id.layoutDatePick);
+        layoutEditPhone = (LinearLayout) fragmentView.findViewById(R.id.layoutEditPhone);
+        cardDatePickLNMP = (CardView) fragmentView.findViewById(R.id.cardPickDateLNMP);
 
         // initialize date to today's date
         textDate.setText(dateFormat.format(today.getTimeInMillis()));
@@ -57,7 +66,22 @@ public class ANCRegister1stFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 // pick date
-                pickEventDate();
+                pickDate(R.id.textDate);
+            }
+        });
+
+        layoutEditPhone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // show edit phone dialog
+                showEditPhoneDialog();
+            }
+        });
+
+        cardDatePickLNMP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pickDate(R.id.textDateLNMP);
             }
         });
 
@@ -66,7 +90,7 @@ public class ANCRegister1stFragment extends Fragment {
     }
 
 
-    private void pickEventDate() {
+    private void pickDate(final int id) {
         // listener
         DatePickerDialog.OnDateSetListener onDateSetListener = new DatePickerDialog.OnDateSetListener() {
             @SuppressWarnings("deprecation")
@@ -74,8 +98,12 @@ public class ANCRegister1stFragment extends Fragment {
             public void onDateSet(DatePickerDialog view, int year, int monthOfYear, int dayOfMonth) {
                 // get picked date
                 // update view
-                Date pickedDate = new Date(year, monthOfYear, dayOfMonth);
-                textDate.setText(dateFormat.format(pickedDate));
+                GregorianCalendar pickedDate = new GregorianCalendar(year, monthOfYear, dayOfMonth);
+                if (id == R.id.textDate)
+                    textDate.setText(dateFormat.format(pickedDate.getTimeInMillis()));
+
+                else if (id == R.id.textDateLNMP)
+                    textDateLNMP.setText(dateFormat.format(pickedDate.getTimeInMillis()));
             }
         };
 
@@ -93,4 +121,47 @@ public class ANCRegister1stFragment extends Fragment {
         datePickerDialog.show(getActivity().getFragmentManager(), "DatePickerDialog");
     }
 
+
+    public void showEditPhoneDialog() {
+        View dialogView = getActivity().getLayoutInflater().inflate(R.layout.layout_dialog_edit_phone, null);
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(getActivity());
+        dialogBuilder.setView(dialogView);
+        dialogBuilder.setCancelable(true);
+
+        final AlertDialog dialog = dialogBuilder.create();
+        dialog.show();
+
+        final EditText editTextPhone = (EditText) dialogView.findViewById(R.id.editTextLocation);
+        // get previously entered location
+        if (textPhone.getText() != null)
+            editTextPhone.setText(textPhone.getText());
+
+        // positive button
+        dialogView.findViewById(R.id.textOk).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String phone = editTextPhone.getText().toString();
+
+                if (TextUtils.isEmpty(phone)) {
+                    editTextPhone.setError("Please enter a valid phone number.");
+                    return;
+                }
+
+                // update view
+                textPhone.setText(phone);
+
+                // close dialog
+                dialog.dismiss();
+            }
+        });
+
+        // negative button
+        dialogView.findViewById(R.id.textCancel).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // close dialog
+                dialog.dismiss();
+            }
+        });
+    }
 }
